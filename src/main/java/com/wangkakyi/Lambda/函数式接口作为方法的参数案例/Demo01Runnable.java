6081649d95c8221258d4ff35877af4bced8d4322
鸢一折折纸 <wky809982944@126.com>
2019-07-24 10:01:21 +0800

    函数式接口作为方法的参数案例

---
+++ src/main/java/com/wangkakyi/Lambda/函数式接口作为方法的参数案例/Demo01Runnable.java
@@ -0,0 +1,22 @@
+package com.wangkakyi.Lambda.函数式接口作为方法的参数案例;
+
+/**
+ * @Author: 鸢一折折纸
+ * @Date: 19-7-24 09:56
+ * @Description:
+ */
+public class Demo01Runnable {
+    public static void startThread(Runnable run) {
+        new Thread(run).start();
+    }
+
+    public static void main(String[] args) {
+        startThread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+"--->"+"线程启动了");
+            }
+        });
+        startThread(()-> System.out.println(Thread.currentThread().getName()+"线程启动了"));
+    }
+}
