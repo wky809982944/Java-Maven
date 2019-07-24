292ae0163fbc84e81b4e6f7e086a436c153bcc1e
鸢一折折纸 <wky809982944@126.com>
2019-07-24 09:52:22 +0800

    使用Lambda优化日志的案例

---
+++ src/main/java/com/wangkakyi/Lambda/性能浪费的日志案例/Demo02Lambda.java
@@ -0,0 +1,26 @@
+package com.wangkakyi.Lambda.性能浪费的日志案例;
+
+/**
+ * @Author: 鸢一折折纸
+ * @Date: 19-7-24 09:20
+ * @Description:
+ * Lambda的特点:延迟加载
+ * Lambda的使用前提,必须存在函数式接口
+ */
+public class Demo02Lambda {
+    public static void showLog(int level, MessageBuilder mb) {
+        if (level == 1) {
+            System.out.println(mb.builderMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String msg1 = "Hello";
+        String msg2 = "World";
+        String msg3 = "Java";
+
+        showLog(1, ()->{
+            return msg1 + msg2 + msg3;
+        });
+    }
+}
---
+++ src/main/java/com/wangkakyi/Lambda/性能浪费的日志案例/MessageBuilder.java
@@ -0,0 +1,11 @@
+package com.wangkakyi.Lambda.性能浪费的日志案例;
+
+/**
+ * @Author: 鸢一折折纸
+ * @Date: 19-7-24 09:21
+ * @Description:
+ */
+@FunctionalInterface
+public interface MessageBuilder {
+    public abstract String builderMessage();
+}
